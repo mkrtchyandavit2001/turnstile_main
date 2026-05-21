3d028d5d59abc423efd6dd605e3dd65a9de883c7
Davit Mkrtchyan <mkrtchyandavit2001@gmail.com>
2026-05-21 10:05:55 +0400

    Check url1

--- app/components/SimilarProductsSwiper/SimilarProductsSwipers.tsx
+++ app/components/SimilarProductsSwiper/SimilarProductsSwipers.tsx
@@ -91,7 +91,7 @@ const SimilarProductsSwipers = ({productCode}) => {
     const fetchData = async () => {
      try {
         const res = await fetch(
-          `${/process.env.NEXT_PUBLIC_API_URL}/api/products`,
+          `productCode/${process.env.NEXT_PUBLIC_API_URL}/api/products`,
           {
             headers: {
               Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
